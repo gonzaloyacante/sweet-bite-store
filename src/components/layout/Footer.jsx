7f3cc4412c19c17d814c8f4c7766acaf8ac11ade
Gonzalo Yacante <gyacante9@gmail.com>
2024-10-13 08:42:20 +0100

    Arreglo de estilos Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,35 +1,28 @@
-import { Box, Text, Flex, Button, Icon } from "@chakra-ui/react";
+import { Box, Text, ButtonGroup, Button, Icon } from "@chakra-ui/react";
 import { FaInstagram, FaFacebookF, FaWhatsapp } from "react-icons/fa";
 
 export const Footer = () => {
   return (
-    <Box bg="background.secondary" p={8}>
-      <Flex
-        direction={{ base: "column", md: "row" }}
-        justify="space-between"
-        align="center"
-        maxW="container.lg"
-        mx="auto">
-        <Box textAlign={{ base: "center", md: "left" }} mb={{ base: 4, md: 0 }}>
-          <Text fontWeight="bold" color="primary.700" fontSize={20}>
-            Sweet Bite
-          </Text>
-          <Text color="text.primary">Calle Pastelería 123, Ciudad Sweet</Text>
-          <Text color="text.primary">Teléfono: (123) 456-7890</Text>
-        </Box>
-        <Flex>
-          <Button variant="ghost" size="lg" onClick={() => {}}>
-            <Icon as={FaInstagram} boxSize={5} color="primary.700" />
-          </Button>
-          <Button variant="ghost" size="lg" onClick={() => {}}>
-            <Icon as={FaFacebookF} boxSize={5} color="primary.700" />
-          </Button>
-          <Button variant="ghost" size="lg" onClick={() => {}}>
-            <Icon as={FaWhatsapp} boxSize={5} color="primary.700" />
-          </Button>
-        </Flex>
-      </Flex>
-      <Text fontSize="sm" color="primary.700" textAlign="center">
+    <Box bg="background.secondary" p={8} textAlign="center">
+      <Box color="text.primary" fontSize={16}>
+        <Text fontWeight="bold" color="primary.700" fontSize={20}>
+          Sweet Bite
+        </Text>
+        <Text>Calle Pastelería 123, Ciudad Sweet</Text>
+        <Text>Teléfono: (123) 456-7890</Text>
+      </Box>
+      <ButtonGroup size="lg" m={2} variant="ghost">
+        <Button onClick={() => {}}>
+          <Icon as={FaInstagram} boxSize={5} color="primary.700" />
+        </Button>
+        <Button onClick={() => {}}>
+          <Icon as={FaFacebookF} boxSize={5} color="primary.700" />
+        </Button>
+        <Button onClick={() => {}}>
+          <Icon as={FaWhatsapp} boxSize={5} color="primary.700" />
+        </Button>
+      </ButtonGroup>
+      <Text fontSize={16} color="primary.700">
         Sweet Bite 2024 © Todos los derechos reservados.
       </Text>
     </Box>
