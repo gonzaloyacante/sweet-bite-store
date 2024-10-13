d3c0b71916328a3336b62f8a4b63d8f6886f89ad
Gonzalo Yacante <gyacante9@gmail.com>
2024-10-13 07:58:53 +0100

    Agregado de LocalStorage en CartContext

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,12 +1,24 @@
 import PropTypes from "prop-types";
-
-import { createContext, useState, useCallback, useMemo } from "react";
+import {
+  createContext,
+  useState,
+  useCallback,
+  useMemo,
+  useEffect,
+} from "react";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(() => {
+    const storedCart = localStorage.getItem("cartItems");
+    return storedCart ? JSON.parse(storedCart) : [];
+  });
   const [isCartOpen, setIsCartOpen] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   const addToCart = useCallback((item) => {
     setCartItems((prevItems) => {
