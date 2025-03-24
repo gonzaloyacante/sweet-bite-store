7a581651da6ca9417404ff23a7d89da9d2c7b30d
gonzaloyacante <gyacante9@gmail.com>
2025-03-24 21:23:28 +0000

    Configurando proyecto

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,29 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   build: {
     outDir: "dist",
-    sourcemap: mode === "development",
-    minify: mode === "production",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-          ui: ["@chakra-ui/react", "@emotion/react", "@emotion/styled"],
-        },
-      },
-    },
   },
-}));
+});
